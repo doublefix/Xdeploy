2775f1c798655008e7a81fda01e733e420ac9003
mahongqin <codepapercut@gmail.com>
2024-08-06 01:39:44 +0800

    Add aliyun certbot

---
+++ ssl/aliyun/certbot-manual-aliyun.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne "0" ]; then
+  echo "This script must be run as root or with sudo." >&2
+  exit 1
+fi
+
+if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+  echo "Usage: $0 <domain> [--dry-run]"
+  exit 1
+fi
+
+DOMAIN=$1
+
+DRY_RUN=""
+if [ "$#" -eq 2 ] && [ "$2" == "--dry-run" ]; then
+  DRY_RUN="--dry-run"
+fi
+
+sudo certbot certonly \
+    --manual --preferred-challenges dns \
+    --manual-auth-hook "alidns" \
+    --manual-cleanup-hook "alidns clean" \
+    -d "*.$DOMAIN" \
+    $DRY_RUN
+
+if [ $? -eq 0 ]; then
+  echo "Certbot command completed successfully."
+else
+  echo "Certbot command failed." >&2
+  exit 1
+fi
+
+# sudo bash ssl/certbot-manual-aliyun.sh example.com --dry-run
--- ssl/script.sh
+++ ssl/script.sh
@@ -8,3 +8,8 @@ nslookup -q=txt _acme-challenge.example.com
 sudo openssl x509 -noout -dates -in /etc/letsencrypt/live/example.com/fullchain.pem
 # 测试续签
 sudo certbot renew --dry-run
+
+
+# aliyun自动续签
+crontab -e
+1 1 */1 * * root certbot renew --manual --preferred-challenges dns --manual-auth-hook "alidns" --manual-cleanup-hook "alidns clean" --deploy-hook "nginx -s reload"

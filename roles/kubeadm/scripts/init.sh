b45118ec58611590317f714f9386c91f80254272
mahongqin <codepapercut@gmail.com>
2024-08-18 20:48:26 +0800

    Fix kubeadm init

--- roles/kubeadm/scripts/init.sh
+++ roles/kubeadm/scripts/init.sh
@@ -1,1 +1,4 @@
-sudo kubeadm init --config=/etc/kubernetes/init-control-plane.yml
+# 生产默认配置
+kubeadm config print init-defaults > default-config.yaml
+# 初始化集群
+kubeadm init --config=/etc/kubernetes/init-control-plane.yml

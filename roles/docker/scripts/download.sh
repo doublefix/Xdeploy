cfc326dc85cf9f90cbfc567884bbe73d51106579
mahongqin <codepapercut@gmail.com>
2024-08-20 00:24:35 +0800

    Add docker

---
+++ roles/docker/scripts/download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -P roles/docker/release https://download.docker.com/linux/static/stable/x86_64/docker-27.1.2.tgz
+wget -P roles/docker/release https://download.docker.com/linux/static/stable/x86_64/docker-rootless-extras-27.1.2.tgz

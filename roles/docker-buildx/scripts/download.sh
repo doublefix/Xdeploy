e75ce495fd1e87d730f7e26b930865e03b4b5fcf
mahongqin <codepapercut@gmail.com>
2024-08-23 10:39:04 +0800

    Fix buildx

--- roles/docker-buildx/scripts/download.sh
+++ roles/docker-buildx/scripts/download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 VERSION="v0.16.2"
-ARCH="linux-amd64"
+ARCH="amd64"
+OS="linux"
 
 if [ ! -z "$1" ]; then
   VERSION=$1
@@ -11,6 +12,10 @@ if [ ! -z "$2" ]; then
   ARCH=$2
 fi
 
+if [ ! -z "$3" ]; then
+  OS=$3
+fi
+
 mkdir -p roles/docker-buildx/release/
 
-wget -O roles/docker-buildx/release/buildx-${VERSION}.${ARCH} https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.${ARCH}
+wget -O roles/docker-buildx/release/buildx-${VERSION}.${OS}-${ARCH} https://github.com/docker/buildx/releases/download/${VERSION}/buildx-${VERSION}.${OS}-${ARCH}

25025c0b161bf19465655d436f44fea51bf48200
mahongqin <codepapercut@gmail.com>
2024-08-23 10:35:37 +0800

    Fix install compose

--- roles/docker-compose/scripts/download.sh
+++ roles/docker-compose/scripts/download.sh
@@ -2,6 +2,7 @@
 
 VERSION="v2.29.2"
 ARCH="x86_64"
+OS="linux"
 
 if [ ! -z "$1" ]; then
   VERSION=$1
@@ -11,6 +12,10 @@ if [ ! -z "$2" ]; then
   ARCH=$2
 fi
 
+if [ ! -z "$3" ]; then
+  OS=$3
+fi
+
 mkdir -p roles/docker-compose/release/${VERSION}/
 
-wget -O roles/docker-compose/release/${VERSION}/docker-compose-linux-${ARCH} https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-linux-${ARCH}
+wget -O roles/docker-compose/release/${VERSION}/docker-compose-${OS}-${ARCH} https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-${OS}-${ARCH}

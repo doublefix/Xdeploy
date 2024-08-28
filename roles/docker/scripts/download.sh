65a37ca52a3e120a8f14c27de8ab43db31124c13
mahongqin <codepapercut@gmail.com>
2024-08-28 23:45:59 +0800

    Fix download script

--- roles/docker/scripts/download.sh
+++ roles/docker/scripts/download.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-wget -P roles/docker/release https://download.docker.com/linux/static/stable/x86_64/docker-27.1.2.tgz
-wget -P roles/docker/release https://download.docker.com/linux/static/stable/x86_64/docker-rootless-extras-27.1.2.tgz
+os="linux"
+arch="x86_64"
+version="27.1.2"
+
+while getopts o:a:v: flag
+do
+    case "${flag}" in
+        o) os=${OPTARG};;
+        a) arch=${OPTARG};;
+        v) version=${OPTARG};;
+    esac
+done
+
+wget -P roles/docker/release https://download.docker.com/${os}/static/stable/${arch}/docker-${version}.tgz
+wget -P roles/docker/release https://download.docker.com/${os}/static/stable/${arch}/docker-rootless-extras-${version}.tgz
+
+# roles/docker/scripts/download.sh -o linux -a arm64 -v 20.10.8

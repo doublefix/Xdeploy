c13ff3b3c6d2af57cd2fe6a9facc195ceaba8f52
mahongqin <codepapercut@gmail.com>
2024-11-01 12:36:40 +0800

    Install nginx

---
+++ roles/normal/nginx.sh
@@ -0,0 +1,24 @@
+# 下载
+wget https://nginx.org/download/nginx-1.24.0.tar.gz
+wget https://gitee.com/mahqi/pubin/releases/download/cla/nginx-1.24.0.tar.gz
+
+# 安装编译依赖
+sudo apt install libpcre3 libpcre3-dev libssl-dev
+
+# 一般编译
+./configure \
+--prefix=/apps/nginx
+
+# 部分实用功能编译
+./configure \
+--prefix=$HOME/nginx \
+--with-http_stub_status_module \
+--with-http_ssl_module \
+--with-http_realip_module \
+--with-http_gzip_static_module \
+--with-stream \
+--user=$USER \
+--group=$USER
+
+# 安装
+make && make install

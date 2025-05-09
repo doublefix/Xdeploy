6588914031e254a7c587c4428b81a6923ed0f1ba
mahongqin <codepapercut@gmail.com>
2025-05-09 17:42:49 +0800

    Add build run ansible params

--- woker/tests/ansible_pyo3.rs
+++ woker/tests/ansible_pyo3.rs
@@ -9,7 +9,7 @@ use uuid::Uuid;
 // export PRIVATE_DATA_DIR=/tmp/ansible_runner
 
 #[derive(Debug, Clone)]
-struct AnsibleRunParams {
+pub struct AnsibleRunParams {
     private_data_dir: String,
     playbook: String,
     cmd: Vec<String>,
@@ -18,22 +18,104 @@ struct AnsibleRunParams {
     quiet: Option<bool>,
 }
 
-impl AnsibleRunParams {
-    fn new(
-        private_data_dir: String,
-        playbook: String,
-        cmd: Vec<String>,
-        verbosity: Option<i32>,
-        quiet: Option<bool>,
-    ) -> Self {
+// 第一阶段：必须设置的参数
+pub struct RequiredParams {
+    private_data_dir: String,
+    playbook: String,
+}
+
+impl RequiredParams {
+    pub fn new(private_data_dir: impl Into<String>, playbook: impl Into<String>) -> Self {
         Self {
-            private_data_dir,
-            playbook,
-            cmd,
-            ident: Uuid::new_v4().to_string(),
-            verbosity,
-            quiet,
+            private_data_dir: private_data_dir.into(),
+            playbook: playbook.into(),
         }
+    }
+
+    pub fn with_cmd(self, cmd: impl Into<Vec<String>>) -> ParamsWithCmd {
+        ParamsWithCmd {
+            private_data_dir: self.private_data_dir,
+            playbook: self.playbook,
+            cmd: cmd.into(),
+        }
+    }
+}
+
+// 第二阶段：设置cmd参数
+pub struct ParamsWithCmd {
+    private_data_dir: String,
+    playbook: String,
+    cmd: Vec<String>,
+}
+
+impl ParamsWithCmd {
+    pub fn build(self) -> AnsibleRunParams {
+        AnsibleRunParams {
+            private_data_dir: self.private_data_dir,
+            playbook: self.playbook,
+            cmd: self.cmd,
+            ident: Uuid::new_v4().to_string(),
+            verbosity: None,
+            quiet: None,
+        }
+    }
+
+    pub fn with_optional(self) -> OptionalParams {
+        OptionalParams {
+            private_data_dir: self.private_data_dir,
+            playbook: self.playbook,
+            cmd: self.cmd,
+            ident: None,
+            verbosity: None,
+            quiet: None,
+        }
+    }
+}
+
+// 第三阶段：可选参数
+pub struct OptionalParams {
+    private_data_dir: String,
+    playbook: String,
+    cmd: Vec<String>,
+    ident: Option<String>,
+    verbosity: Option<i32>,
+    quiet: Option<bool>,
+}
+
+impl OptionalParams {
+    pub fn ident(mut self, ident: impl Into<String>) -> Self {
+        self.ident = Some(ident.into());
+        self
+    }
+
+    pub fn verbosity(mut self, verbosity: i32) -> Self {
+        self.verbosity = Some(verbosity);
+        self
+    }
+
+    pub fn quiet(mut self, quiet: bool) -> Self {
+        self.quiet = Some(quiet);
+        self
+    }
+
+    pub fn build(self) -> AnsibleRunParams {
+        AnsibleRunParams {
+            private_data_dir: self.private_data_dir,
+            playbook: self.playbook,
+            cmd: self.cmd,
+            ident: self.ident.unwrap_or_else(|| Uuid::new_v4().to_string()),
+            verbosity: self.verbosity,
+            quiet: self.quiet,
+        }
+    }
+}
+
+impl AnsibleRunParams {
+    pub fn builder(
+        private_data_dir: impl Into<String>,
+        playbook: impl Into<String>,
+    ) -> RequiredParams {
+        RequiredParams::new(private_data_dir, playbook)
     }
 
     fn to_py_dict<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyDict>> {
@@ -82,13 +164,20 @@ async fn run_ansible(params: AnsibleRunParams) -> PyResult<()> {
 
 #[tokio::test]
 async fn test_ansible_pyo3() -> PyResult<()> {
-    let params = AnsibleRunParams::new(
+    let params_full = AnsibleRunParams::builder(
         env::var("PRIVATE_DATA_DIR").expect("PRIVATE_DATA_DIR not set"),
-        "playbooks/cmd.yml".to_string(),
-        vec!["echo".to_string(), "Hello".to_string(), "World".to_string()],
-        Some(1),
-        None,
-    );
+        "playbooks/cmd.yml",
+    )
+    .with_cmd(vec![
+        "echo".to_string(),
+        "Hello".to_string(),
+        "World".to_string(),
+    ])
+    .with_optional()
+    .ident(Uuid::new_v4().to_string())
+    .verbosity(1)
+    // .quiet(true)
+    .build();
 
-    run_ansible(params).await
+    run_ansible(params_full).await
 }

1dc814ff12de347ef94ea96877296b225ef901b3
mahongqin <codepapercut@gmail.com>
2025-05-13 00:09:54 +0800

    Add test run ansible

--- tests/run_ansible.py
+++ tests/run_ansible.py
@@ -3,22 +3,33 @@ import uuid
 import ansible_runner
 
 run_uuid = str(uuid.uuid4())
+
+inventory_dict = {
+    "servers": {
+        "hosts": {
+            "debian-root": {"ansible_user": "root"},
+            "rocky-root": {"ansible_user": "root"},
+            "ubuntu-root": {"ansible_user": "root"},
+        }
+    }
+}
 print(f"Generated UUID for this run: {run_uuid}")
 
 r = ansible_runner.run(
     private_data_dir=".",
     playbook="playbooks/cmd.yml",
     extravars={"cmd": ["echo", "Hello", "World"]},
+    inventory=inventory_dict,
     ident=run_uuid,
     quiet=True,
-    # verbosity=1,
+    verbosity=1,
 )
 
 # 打印状态和事件输出
-# print(f"Status: {r.status}")
-# print(f"RC: {r.rc}")
+print(f"Status: {r.status}")
+print(f"RC: {r.rc}")
 
-# print("\n--- STDOUT ---")
-# for ev in r.events:
-#     if "stdout" in ev:
-#         print(ev["stdout"])
+print("\n--- STDOUT ---")
+for ev in r.events:
+    if "stdout" in ev:
+        print(ev["stdout"])

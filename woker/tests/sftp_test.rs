e7365f83bb15a9703e424a2210aedd2a1716cc46
mahongqin <codepapercut@gmail.com>
2025-05-20 17:40:39 +0800

    Update file to target server

--- woker/tests/sftp_test.rs
+++ woker/tests/sftp_test.rs
@@ -56,10 +56,7 @@ fn upload_folder(
 ) -> Result<(), Box<dyn std::error::Error>> {
     let sftp = sess.sftp()?;
 
-    // Create remote folder if it doesn't exist
-    if sftp.stat(remote_folder).is_err() {
-        sftp.mkdir(remote_folder, 0o755)?;
-    }
+    ensure_remote_dir(&sftp, remote_folder)?;
 
     for entry in fs::read_dir(local_folder)? {
         let entry = entry?;
@@ -67,10 +64,8 @@ fn upload_folder(
         let remote_path = remote_folder.join(entry.file_name());
 
         if path.is_dir() {
-            // Recursively upload subdirectories
             upload_folder(sess, &path, &remote_path)?;
         } else {
-            // Upload files
             let mut local_file = fs::File::open(&path)?;
             let mut remote_file = sftp.create(&remote_path)?;
 
@@ -82,6 +77,27 @@ fn upload_folder(
         }
     }
 
+    Ok(())
+}
+
+fn ensure_remote_dir(
+    sftp: &ssh2::Sftp,
+    remote_dir: &Path,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut current = std::path::PathBuf::from("/");
+    for part in remote_dir.components() {
+        current = current.join(part);
+        if sftp.stat(&current).is_err() {
+            match sftp.mkdir(&current, 0o755) {
+                Ok(_) => {}
+                Err(e) => {
+                    if e.code() != ssh2::ErrorCode::Session(-31) {
+                        return Err(Box::new(e));
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
 
@@ -101,9 +117,10 @@ fn test_sftp() -> Result<(), Box<dyn std::error::Error>> {
 
     let sess = connect_ssh(&config)?;
 
-    let folder_name = "jupyter";
-    let target_path = "/root";
-    let source_path_with_folder_name = format!("{home}/{folder_name}");
+    let source_path = "/var/tmp/chess";
+    let folder_name = "ee65adc925d6d5acd33beeba4747f90fda68bec1dbea6a1dea16691fe9fdfeb8";
+    let target_path = "/tmp/.chess";
+    let source_path_with_folder_name = format!("{source_path}/{folder_name}");
     let target_path_with_folder_name = format!("{target_path}/{folder_name}");
 
     let local_path = Path::new(&source_path_with_folder_name);
